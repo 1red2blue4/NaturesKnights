06037054f185c8b7d91fa7465b2424e92b438b18
Mark Obeldobel <mjo5501@rit.edu>
2016-11-26 20:10:26 -0500

    Added quite a bit, but hit a rock.

--- src/server.js
+++ src/server.js
@@ -1,29 +1,85 @@
 const http = require('http');
 const fs = require('fs');
 const socketio = require('socket.io');
-// const path = require('path');
-//const canvas = require('canvas');
+//const express = require('express');
+const path = require('path');
 
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+//const expressApp = express();
+
+const port = process.env.PORT || process.env.NODE_PORT || 3500;
 
 // read the client html file into memory
 // _dirname in node is the current directory
 // (in this case the same folder as the server js file)
-const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 
-//const image = new canvas.Image();
-//image.src = '../media/ghost.png';
+//file names in the client folder
+const fileNames = ['/index.html', '/media/ghost.png', '/media/logo.png'];
+
+const cachedFiles = {};
+
+for (let i = 0; i < fileNames.length; i++){
+  const currentName = fileNames[i];
+  const resolvedPath = path.resolve(`${__dirname}/../client/${fileNames[i]}`);
+  cachedFiles[currentName] = fs.readFileSync(resolvedPath);
+}
 
 const onRequest = (request, response) => {
+  /*
+  if (action === index) {
+    response.writeHead(200, { 'Content-type': 'text/html' });
+    response.write(index);
+    response.end();
+  } else if (action === testImage) {
+    response.writeHead(200, { 'Content-type': 'image/png' });
+    response.write(testImage);
+    response.end();
+  }
+  */
+
+  /*
+  expressApp.use(express.static('media'));
+
+
+  expressApp.get('/', (req, res) => {
+    res.sendfile(path.resolve('client/index.html'));
+  });
+
+
+  expressApp.get('/ghost', (req, res) => {
+    res.sendfile(path.resolve('media/ghost.png'));
+  });
+  */
+  
+  console.log(request.url);
+  
+  if (request.url === "/")
+  {
+    response.writeHead(200);
+    response.end(cachedFiles['/index.html']);
+  }
+  
+  if (fileNames.indexOf(request.url) > -1) {
+    response.writeHead(200);
+    response.end(cachedFiles[request.url]);
+  }
+  
+  /*
   const contentType = response.getHeader('content-type');
   response.writeHead(200, { 'Content-Type': contentType });
-  response.write(index);
-  response.end();
+
+  if (request.url === index) {
+    response.write(index);
+  }  else if (request.url === testImage) {
+    response.write(testImage);
+  }
+  */
+
+  //response.end();
 };
 
 const app = http.createServer(onRequest).listen(port);
 
-console.log(`Listening on 127.0.0.1: ${port}`);
+console.log(`Listening on 127.0.0.1:${port}`);
 
 /*
 function getRandNum(min, max) {
@@ -34,18 +90,28 @@ function getRandNum(min, max) {
 
 // pass in the http server into socketio and grab the websocket server as io
 const io = socketio(app);
-let userCount = 0;
+let setRoom = 0;
 const users = {};
+const openRooms = {};
+const allTeams = {};
 
 const onJoined = (sock) => {
   const socket = sock;
-  userCount += 1;
-  const yourTicketNum = userCount;
+  setRoom += 1;
+  const yourTicketNum = setRoom;
   users[yourTicketNum] = yourTicketNum;
   const roomNum = Math.floor((yourTicketNum + 1) / 2);
   console.log(roomNum);
   socket.join(`room${roomNum}`);
-  socket.emit('getTicketNum', { ticket: yourTicketNum, room: roomNum, /*testImage: image*/ });
+  if (!openRooms[roomNum]) {
+    openRooms[roomNum] = roomNum;
+  }
+  socket.emit('getTicketNum', { ticket: yourTicketNum, room: roomNum });
+  
+  
+  socket.on("findBothTeams", (data) => {
+    io.to(`room${roomNum}`).emit("receiveEnemyTeam", {team: data.team});
+  })
 };
 
 io.sockets.on('connection', (sock) => {
@@ -53,7 +119,6 @@ io.sockets.on('connection', (sock) => {
   onJoined(sock);
 
   sock.on('disconnect', () => {
-    userCount -= 1;
     console.log('ended');
   });
 });
